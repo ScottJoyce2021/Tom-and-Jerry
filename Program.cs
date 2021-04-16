60d767938205d89fd1ff86e936e88058ed6cfc4b
Scott Joyce <sjoyce2021@jpkeefehs.org>
2021-04-16 12:05:18 -0400

    Finished count streaks with dictionary

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ namespace compressions
 {
     class Program
     {
+        static Dictionary<byte, byte> myDictionary = new Dictionary<byte, byte>();
         static void Main(string[] args)
         {
             var list = new List<int>();
             char header = ' ';
-            string fileName = @"C:\Users\scjoy\source\repos\Tom and Jerry\bin\Dice.pgm";
+            string fileName = @"C:\Users\scjoy\source\repos\Tom and Jerry\bin\Tom_Jerry.pgm";
             {
                 byte[] line;
                 line = File.ReadAllBytes(fileName);
@@ -21,11 +22,51 @@ namespace compressions
                     if (line[i] > 127) { break; }
                     Console.Write(header);
                 }
+                createDictionary(line);
                 comppress(line);
                 deCompress(line);
             }
 
             Console.ReadKey();
+        }
+        static void createDictionary(byte[] arr) 
+        {
+            int streaks = 1;
+            byte grayscaleVal = arr[16];
+            int[] saveStreak = new int [256];
+            for(int i = 17; i < arr.Length; i++)
+            {
+                if(arr[i] == grayscaleVal)
+                {
+                    streaks++;
+                    if(streaks == 3)
+                    {
+                        saveStreak[grayscaleVal]++;
+                        streaks = 0;
+                    }
+                }
+                else
+                {
+                    streaks = 1;
+                    grayscaleVal = arr[i];
+                }
+                
+            } 
+            for(int j = 0; j < 256; j++)
+            {
+                int highIndex = 0;
+                int high = -1;
+                for(int h = 0; h < 256; h++)
+                {
+                    if(saveStreak[h] > high)
+                    {
+                        high = saveStreak[h];
+                        highIndex = h;
+                    }
+                }
+                myDictionary.Add((byte)highIndex, (byte)j);
+                saveStreak[highIndex] = -2;
+            }
         }
         static int comppress(byte[] arr)
         {
